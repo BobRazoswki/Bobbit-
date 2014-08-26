582d30b946cef2e13157ac0cc74f014623cd27ea
BobRazoswki <bobongithub@gmail.com>
2014-08-26 16:44:55 +0100

    83.23%

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,8 +3,7 @@ class LikesController < ApplicationController
 	before_action :authenticate_user!, except: [:index]
 
 	def create
-		@new_post = Post.find(params[:post_id])
-		@new_post.likes.create
+		@new_post = Post.find(params[:post_id]).likes.create
 		render json: { new_like_count: @new_post.likes.count, new_sentence: @new_post.pluralize_likes }
 	end 
 
--- spec/features/categories_feature_spec.rb
+++ spec/features/categories_feature_spec.rb
@@ -17,4 +17,19 @@ describe 'CATEGORIES' do
 		end
 	end
 
+	context 'controversial' do
+		it 'displays the hottest post' do
+				
+
+				alex = User.create(
+					email: "a@a.fr",
+					password: "123456789",
+					password_confirmation: "123456789")
+				login_as("a@a.fr", "123456789")
+
+			visit posts_hot_path
+			expect(page).to have_content('Controversial')
+		end
+	end
+
 end
